fdb3af29acbe773a80e40106bdfc057a8208bbf7
hashcode55 <blackdragon0909@gmail.com>
2016-05-27 00:07:49 +0530

    fixed lra

--- Lra.java
+++ Lra.java
@@ -14,26 +14,33 @@ public class LRA {
     static int max = -1;
     public static void main(String args[]){
         int arr[] = {6, 2, 5, 4, 5, 1, 6};
-        findMaxRect(arr, 0, 6);
+        max = findMaxRect(arr, 0, 6);
+        System.out.println(max);
     }
     
-    public static void findMaxRect(int arr[], int i, int j){
+    public static int findMaxRect(int arr[], int i, int j){
         if(i <= j){
             int k = findMin(arr, i, j);
-            findMaxRect(arr, i, k - 1);
-            findMaxRect(arr, k + 1, j);
-            findMaxArea(arr, i, k, j);
+            return max(findMaxRect(arr, i, k - 1), findMaxRect(arr, k + 1, j), arr[k] * (j-i+1));
         }
+        return 0;
     }
     
-    public static void findMaxArea(int arr[], int i, int k, int j){
-        
+    public static int max(int i, int j, int k){
+        int max = -1;
+        if(i > j)
+            max = i;
+        else max = j;
+        if(k > max) max = k;
+        return max;
     }
+    
+  
     
     public static int findMin(int arr[], int i, int j){
         int min = Integer.MAX_VALUE;
         int in = 0;
-        for(int k = i; k < j; k++) { if (arr[i] < min) min = arr[i]; in = i;}
+        for(int k = i; k <= j; k++) { if (arr[k] < min) {min = arr[k]; in = k;}}
         return in;
     }
 }
